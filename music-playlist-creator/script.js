443d370c1b828eb8da7e8ef374b60fecdc6d3bff
Izuchukwu Mba <izuchukwu@meta.com>
2024-06-05 16:55:25 -0700

    modal done!

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -2,9 +2,13 @@ let playlistData = data
 
 let playlists = document.getElementById('playlists');
 
-const playlistContainer = playlistData['playlists'].map((item) => {
+let playlist_image = document.querySelectorAll('.playlist-image');
+
+const playlistContainer = playlistData['playlists'].map((item, index1) => {
+
+    playlist_image.src = item['playlist_art'];
     return `
-    <div class="playlist-cards" onclick = "openModal(${item, item['songs']})">
+    <div class="playlist-card playlist-${index1}" id ="${index1}">
         <div class='image-container'><img class="playlist-image" src = "${item['playlist_art']}"></div>
 
         <div class = 'playlist-text-container'>
@@ -17,21 +21,59 @@ const playlistContainer = playlistData['playlists'].map((item) => {
         </div>
 
     </div>`;
+
+
+
 }).join('')
 
 playlists.innerHTML = playlistContainer
 
-let modal = document.getElementById('modalContainer');
-let close_icon = document.querySelector('.close');
+let songList = document.getElementById('modal-song-info');
+let modal = document.getElementById('modal-container');
+let closeBtn = document.querySelector('.close-icon');
 
 function openModal(playlist, song){
     document.querySelector('.modal-playlist-image').src = playlist['playlist_art'];
-    document.querySelector('.modal-playlist-title').innerText = playlist['playlist_name'];
-    document.querySelector('.modal-playlist-creator').innerText = playlist['playlist_creator'];
-    document.querySelector('.song-title').innerText = song['title'];
-    document.querySelector('.song-artist-name').innerText = song['artist'];
-    document.querySelector('.album-name').innerText = song['album'];
-    document.querySelector('.song-duration').innerHTML = song['song-duration'];
+    document.querySelector('.modal-playlist-title').innerHTML = playlist['playlist_name'];
+    document.querySelector('.modal-playlist-creator-name').innerHTML = playlist['playlist_creator'];
 
-    modal.style.display = "block";
+    let songContainer = song.map((item, index) => {
+        return `
+            <div class="song-container">
+            <img src="" class="song-img"/>
+            <div class="song-text">
+                <div class="song-title">${item['title']}</div>
+                <div class="song-artist-name">${item['artist']}</div>
+                <div class ='album-name'>${item['album']}</div>
+            </div>
+            <div class="song-duration">${item['duration']}</div>
+            </div>
+
+            `
+
+    }).join('')
+
+    songList.innerHTML = songContainer
+
+    modal.style.visibility = "visible";
+    console.log('modal opened');
 }
+
+function closeModal(){
+    modal.style.visibility = "hidden";
+    document.querySelector('.modal-playlist-image').src = '';
+
+}
+
+playlists.addEventListener('click', (event) => {
+
+    let playlist_index = event.target.closest('.playlist-card').id
+
+    if(event.target.closest('.playlist-card')){
+
+        openModal(playlistData.playlists[playlist_index], playlistData.playlists[playlist_index]['songs']);
+
+    }})
+
+
+closeBtn.addEventListener('click', closeModal);
